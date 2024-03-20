cf99f771625a1d59e0e96d819177c1ad343d9e96
huangjianmei <920287501@qq.com>
2024-03-20 12:12:24 +0800

    通过外部样式类修改组件样式

---
+++ components/custom06/custom06.js
@@ -0,0 +1,24 @@
+// components/custom06/custom06.js
+Component({
+    externalClasses:["extend-class"],
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
